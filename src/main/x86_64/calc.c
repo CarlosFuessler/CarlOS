6d8b976a617c5e839a14222ebd61be481144f5b0
Carlos <181351713+CarlosFuessler@users.noreply.github.com>
2025-12-15 08:53:00 +0100

    fix

--- src/main/x86_64/calc.c
+++ src/main/x86_64/calc.c
@@ -2,152 +2,135 @@
 
 #include "calc.h"
 #include "keyboard.h"
-#include "string.h"
-#include "print.h"
 #include "memory_management.h"
+#include "print.h"
+#include "string.h"
 
 #include <stdint.h>
 
-struct equation
-{
-    uint8_t number[16];
-    uint8_t operation; // 1=+, 2=-, 3=*, 4=/
+struct equation {
+  uint8_t number[16];
+  uint8_t operation; // 1=+, 2=-, 3=*, 4=/
 };
 
-void calc_input(void)
-{
-    struct equation terms[100];
-    unsigned char input = 0;
-    uint8_t term_index = 0;
-    uint8_t digit_index = 0;
+void calc_input(void) {
+  struct equation terms[100];
+  unsigned char input = 0;
+  uint8_t term_index = 0;
+  uint8_t digit_index = 0;
 
-    print_clear();
-    print_str("##########################\n");
-    print_str("####### Calculator #######\n");
-    print_str("##########################\n\n");
+  print_clear();
+  print_str("##########################\n");
+  print_str("####### Calculator #######\n");
+  print_str("##########################\n\n");
 
-    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Enter numbers and operations (= to calculate)\n");
+  print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+  print_str("Enter numbers and operations (= to calculate)\n");
 
-    while (term_index < 99)
-    {
+  while (term_index < 99) {
 
-        print_str("Number ");
-        char num_str[8];
-        int_to_string(term_index + 1, num_str);
-        print_str(num_str);
-        print_str(": ");
+    print_str("Number ");
+    char num_str[8];
+    int_to_string(term_index + 1, num_str);
+    print_str(num_str);
+    print_str(": ");
 
-        digit_index = 0;
+    digit_index = 0;
 
-        while (1)
-        {
-            input = keyboard_get_char();
+    while (1) {
+      input = keyboard_get_char();
 
-            if (input == '=')
-            {
-                if (digit_index > 0)
-                {
-                    terms[term_index].number[digit_index] = '\0';
-                    term_index++;
-                }
-                goto calculate;
-            }
-
-            if (input == KEY_ENTER && digit_index > 0)
-            {
-                terms[term_index].number[digit_index] = '\0';
-                print_newline();
-                break;
-            }
-
-            if (input >= '0' && input <= '9' && digit_index < 15)
-            {
-                terms[term_index].number[digit_index] = input;
-                digit_index++;
-                print_char(input);
-            }
+      if (input == '=') {
+        if (digit_index > 0) {
+          terms[term_index].number[digit_index] = '\0';
+          term_index++;
         }
+        goto calculate;
+      }
 
-        print_str("Operation (1=+ , 2=- , 3=* , 4=/ , = (finish): ");
+      if (input == KEY_ENTER && digit_index > 0) {
+        terms[term_index].number[digit_index] = '\0';
+        print_newline();
+        break;
+      }
 
-        while (1)
-        {
-            input = keyboard_get_char();
-
-            if (input == '=')
-            {
-                term_index++;
-                goto calculate;
-            }
-
-            if (input >= '1' && input <= '4')
-            {
-                terms[term_index].operation = input - '0';
-                print_char(input);
-                print_newline();
-                break;
-            }
-        }
-
-        term_index++;
+      if (input >= '0' && input <= '9' && digit_index < 15) {
+        terms[term_index].number[digit_index] = input;
+        digit_index++;
+        print_char(input);
+      }
     }
+
+    print_str("Operation (1=+ , 2=- , 3=* , 4=/ , = (finish): ");
+
+    while (1) {
+      input = keyboard_get_char();
+
+      if (input == '=') {
+        term_index++;
+        goto calculate;
+      }
+
+      if (input >= '1' && input <= '4') {
+        terms[term_index].operation = input - '0';
+        print_char(input);
+        print_newline();
+        break;
+      }
+    }
+
+    term_index++;
+  }
 
 calculate:
-    if (term_index == 0)
-    {
-        print_str("No numbers entered!\n");
+  if (term_index == 0) {
+    print_str("No numbers entered!\n");
+    return;
+  }
+
+  print_str("\n");
+  print_str("Calculation: ");
+  int result = _stoi((char *)terms[0].number);
+  print_str((char *)terms[0].number);
+
+  for (int i = 1; i < term_index; i++) {
+    int next_number = _stoi((char *)terms[i].number);
+
+    switch (terms[i - 1].operation) {
+    case 1:
+      print_str(" + ");
+      result += next_number;
+      break;
+    case 2:
+      print_str(" - ");
+      result -= next_number;
+      break;
+    case 3:
+      print_str(" * ");
+      result *= next_number;
+      break;
+    case 4:
+      print_str(" / ");
+      if (next_number != 0)
+        result /= next_number;
+      else {
+        print_str("ERROR(div by 0)");
         return;
+      }
+      break;
     }
 
-    // berechne
-    print_str("\n");
-    print_str("Calculation: ");
-    int result = _stoi((char *)terms[0].number);
-    print_str((char *)terms[0].number);
+    print_str((char *)terms[i].number);
+  }
 
-    for (int i = 1; i < term_index; i++)
-    {
-        int next_number = _stoi((char *)terms[i].number);
-
-        switch (terms[i - 1].operation)
-        {
-        case 1:
-            print_str(" + ");
-            result += next_number;
-            break;
-        case 2:
-            print_str(" - ");
-            result -= next_number;
-            break;
-        case 3:
-            print_str(" * ");
-            result *= next_number;
-            break;
-        case 4:
-            print_str(" / ");
-            if (next_number != 0)
-                result /= next_number;
-            else
-            {
-                print_str("ERROR(div by 0)");
-                return;
-            }
-            break;
-        }
-
-        print_str((char *)terms[i].number);
-    }
-
-    print_str(" = ");
-    char result_str[16];
-    int_to_string(result, result_str);
-    print_str(result_str);
-    print_str("\n\n");
+  print_str(" = ");
+  char result_str[16];
+  int_to_string(result, result_str);
+  print_str(result_str);
+  print_str("\n\n");
 }
 
-void calc(void)
-{
-    char buf_a[8], buf_b[8];
-    calc_input();
+void calc(void) {
+  char buf_a[8], buf_b[8];
+  calc_input();
 }
--- src/main/x86_64/string.c
+++ src/main/x86_64/string.c
@@ -3,149 +3,126 @@
 typedef bool;
 #define true 1
 #define false 0
-size_t strlen(const char *str)
-{
-    size_t length = 0;
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-    return length;
+size_t strlen(const char *str) {
+  size_t length = 0;
+  while (str[length] != '\0') {
+    length++;
+  }
+  return length;
 }
 
-int strcmp(const char *str1, const char *str2)
-{
-    size_t i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0')
-    {
-        if (str1[i] != str2[i])
-        {
-            return (str1[i] < str2[i]) ? -1 : 1;
-        }
-        i++;
+int strcmp(const char *str1, const char *str2) {
+  size_t i = 0;
+  while (str1[i] != '\0' && str2[i] != '\0') {
+    if (str1[i] != str2[i]) {
+      return (str1[i] < str2[i]) ? -1 : 1;
     }
+    i++;
+  }
 
-    if (str1[i] == str2[i])
-        return 0;
-    return (str1[i] == '\0') ? -1 : 1;
+  if (str1[i] == str2[i])
+    return 0;
+  return (str1[i] == '\0') ? -1 : 1;
 }
 
-char *_strcpy(char *dest, const char *src)
-{
-    size_t i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return dest;
+char *_strcpy(char *dest, const char *src) {
+  size_t i = 0;
+  while (src[i] != '\0') {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
+  return dest;
 }
 
-char *_strcat(char *dest, const char *src)
-{
-    size_t dest_len = strlen(dest);
-    size_t i = 0;
+char *_strcat(char *dest, const char *src) {
+  size_t dest_len = strlen(dest);
+  size_t i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-    dest[dest_len + i] = '\0';
-    return dest;
+  while (src[i] != '\0') {
+    dest[dest_len + i] = src[i];
+    i++;
+  }
+  dest[dest_len + i] = '\0';
+  return dest;
 }
 
-void int_to_string(int value, char *buffer)
-{
-    if (value == 0)
-    {
-        buffer[0] = '0';
-        buffer[1] = '\0';
-        return;
-    }
-
-    int is_negative = (value < 0);
-    if (is_negative)
-        value = -value;
-
-    int index = 0;
-    while (value > 0)
-    {
-        buffer[index++] = '0' + (value % 10);
-        value /= 10;
-    }
-
-    if (is_negative)
-        buffer[index++] = '-';
-    buffer[index] = '\0';
-    for (int i = 0; i < index / 2; i++)
-    {
-        char temp = buffer[i];
-        buffer[i] = buffer[index - 1 - i];
-        buffer[index - 1 - i] = temp;
-    }
-}
-
-void hex_to_string(unsigned int value, char *buffer)
-{
-    const char hex_digits[] = "0123456789ABCDEF";
+void int_to_string(int value, char *buffer) {
+  if (value == 0) {
     buffer[0] = '0';
-    buffer[1] = 'x';
+    buffer[1] = '\0';
+    return;
+  }
 
-    if (value == 0)
-    {
-        buffer[2] = '0';
-        buffer[3] = '\0';
-        return;
-    }
+  int is_negative = (value < 0);
+  if (is_negative)
+    value = -value;
 
-    int index = 2;
-    char temp[16];
-    int temp_index = 0;
+  int index = 0;
+  while (value > 0) {
+    buffer[index++] = '0' + (value % 10);
+    value /= 10;
+  }
 
-    while (value > 0)
-    {
-        temp[temp_index++] = hex_digits[value % 16];
-        value /= 16;
-    }
-
-    // Reverse and copy
-    for (int i = temp_index - 1; i >= 0; i--)
-    {
-        buffer[index++] = temp[i];
-    }
-    buffer[index] = '\0';
+  if (is_negative)
+    buffer[index++] = '-';
+  buffer[index] = '\0';
+  for (int i = 0; i < index / 2; i++) {
+    char temp = buffer[i];
+    buffer[i] = buffer[index - 1 - i];
+    buffer[index - 1 - i] = temp;
+  }
 }
 
-int _stoi(const char *str)
-{
-    if (str == NULL)
-        return 0;
+void hex_to_string(unsigned int value, char *buffer) {
+  const char hex_digits[] = "0123456789ABCDEF";
+  buffer[0] = '0';
+  buffer[1] = 'x';
 
-    int res = 0;
-    bool negative = false;
+  if (value == 0) {
+    buffer[2] = '0';
+    buffer[3] = '\0';
+    return;
+  }
 
-    while (*str == ' ')
-    {
-        str++;
-    }
+  int index = 2;
+  char temp[16];
+  int temp_index = 0;
 
-    if (*str == '-')
-    {
-        negative = true;
-        str++;
-    }
-    else if (*str == '+')
-    {
-        str++;
-    }
+  while (value > 0) {
+    temp[temp_index++] = hex_digits[value % 16];
+    value /= 16;
+  }
 
-    while (*str >= '0' && *str <= '9')
-    {
-        res = res * 10 + (*str - '0');
-        str++;
-    }
+  // Reverse and copy
+  for (int i = temp_index - 1; i >= 0; i--) {
+    buffer[index++] = temp[i];
+  }
+  buffer[index] = '\0';
+}
 
-    return negative ? -res : res;
+int _stoi(const char *str) {
+  if (str == NULL)
+    return 0;
+
+  int res = 0;
+  bool negative = false;
+
+  while (*str == ' ') {
+    str++;
+  }
+
+  if (*str == '-') {
+    negative = true;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+
+  while (*str >= '0' && *str <= '9') {
+    res = res * 10 + (*str - '0');
+    str++;
+  }
+
+  return negative ? -res : res;
 }
